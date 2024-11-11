f38598301477621cc4ce46f4b716ce06bd4a5dcd
RomainP <romainportier7@gmail.com>
2024-11-11 14:46:34 +0100

    add basic conenction page

---
+++ front/src/app/chat/chat.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-chat',
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
+  ],
+  templateUrl: './chat.component.html',
+  styleUrl: './chat.component.css'
+})
+export class ChatComponent {
+  name: string = '';
+  connected: boolean = false;
+
+  connect() {
+    this.connected = true;
+  }
+}
